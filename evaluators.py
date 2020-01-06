380c17504cddb77c46b94bb713ec2360462be42c
Lukas Schwab <lukas.schwab@gmail.com>
2020-01-05 17:46:12 -0800

    Add some comments for evaluators

--- evaluators.py
+++ evaluators.py
@@ -1,17 +1,25 @@
-# Just copies the same board.
+# GENERIC
+
+# copy_evaluator does not change any cell states.
 def copy_evaluator(board, i):
     return board.get_cell_state(i)
 
+# invert_evaluator inverts every cell state.
 def invert_evaluator(board, i):
     return not copy_evaluator(board, i)
 
+# infection_evaluator makes grants life to every cell neighboring at least one
+# live cell.
 def infection_evaluator(board, i):
     neighbors = board.grid._get_neighbors(i)
     n_states = sum([board.get_cell_state(n) for n in neighbors])
     return n_states > 0 or board.get_cell_state(i)
 
-# Adapted conway for the default TriGrid.
-def conwayish_evaluator(board, i):
+# TRIANGLE GRIDS
+
+# trigrid_conway_evaluator is a shoddy adapted version of the standard
+# conway_evaluator for the three-adjacent triangle grid.
+def trigrid_conway_evaluator(board, i):
     neighbors = board.grid._get_neighbors(i)
     n_states = sum([board.get_cell_state(n) for n in neighbors])
     # Unchanged if 1
@@ -19,6 +27,8 @@ def conwayish_evaluator(board, i):
         return board.get_cell_state(i)
     # Spawn if 2; else die
     return n_states == 2
+
+# QUAD GRIDS
 
 # Standard Game of Life.
 def conway_evaluator(board, i):
